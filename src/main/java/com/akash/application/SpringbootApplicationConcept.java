99fab5c9f212f67a1f51026d24336e1923ff43b7
Akashec1007 <99259624+Akashec1007@users.noreply.github.com>
2026-04-14 23:46:56 +0530

    commit changes

--- src/main/java/com/akash/application/SpringbootApplicationConcept.java
+++ src/main/java/com/akash/application/SpringbootApplicationConcept.java
@@ -1,10 +1,14 @@
 package com.akash.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @SpringBootApplication
 public class SpringbootApplicationConcept {
 	public static void main(String[] args) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println("Spring Boot Application Execution Start Triggers @ " +LocalDateTime.now().format(formatter));
 		SpringApplication.run(SpringbootApplicationConcept.class, args);
-		System.out.println("Spring Boot Application Execution Started");
+		System.out.println("Spring Boot Application Execution Strted @ " +LocalDateTime.now().format(formatter));
 	}
 }
