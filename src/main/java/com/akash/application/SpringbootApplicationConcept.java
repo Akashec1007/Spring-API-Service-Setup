55ddfbb54ab11948f70d33be58b10dcf1d43b1e5
Akashec1007 <99259624+Akashec1007@users.noreply.github.com>
2026-04-15 01:07:39 +0530

    Update SpringbootApplicationConcept.java

--- src/main/java/com/akash/application/SpringbootApplicationConcept.java
+++ src/main/java/com/akash/application/SpringbootApplicationConcept.java
@@ -3,12 +3,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class SpringbootApplicationConcept {
 	public static void main(String[] args) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		System.out.println("Spring Boot Application Execution Start Triggers @ " +LocalDateTime.now().format(formatter));
+		System.out.println("Spring Boot Application Execution Start Triggers @ "+LocalDateTime.now().format(formatter));
 		SpringApplication.run(SpringbootApplicationConcept.class, args);
-		System.out.println("Spring Boot Application Execution Started @ " +LocalDateTime.now().format(formatter));
+		System.out.println("Spring Boot Application Execution Started @ "+LocalDateTime.now().format(formatter));
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/hello";
+		String response = restTemplate.getForObject(url, String.class);
+		System.out.println("Spring Boot Application Self Call Response "+response);
 	}
 }
