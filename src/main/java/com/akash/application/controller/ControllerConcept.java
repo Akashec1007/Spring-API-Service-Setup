4bb47857e8321eed68d3cfab0ffcdba755d974df
Akashec1007 <99259624+Akashec1007@users.noreply.github.com>
2026-04-14 22:21:04 +0530

    Commit Java downgrade to 17 to compile on github

--- src/main/java/com/akash/application/controller/ControllerConcept.java
+++ src/main/java/com/akash/application/controller/ControllerConcept.java
@@ -3,6 +3,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.akash.application.service.ServiceConcept;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @RestController
 public class ControllerConcept {
 	@Autowired
@@ -16,4 +18,11 @@ public class ControllerConcept {
 	public String pingTest() {
 		return "Service is up and running.";
 	}
+
+	@GetMapping("/time")
+	public String getCurrentTime() {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			return LocalDateTime.now().format(formatter);
+	}
+
 }
